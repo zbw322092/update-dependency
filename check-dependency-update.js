3165ba491b09cfd439855ce71bdb5f65c9594601
Bowen Zhong <zbw322092@hotmail.com>
2019-03-13 16:27:52 +0800

    check update

---
+++ check-dependency-update.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const childProcessExec = require('child_process').exec;
+const util = require('util');
+const path = require('path');
+
+const exec = util.promisify(childProcessExec);
+const packageJsonPath = path.join(process.cwd(), './package.json');
+
+checkPackageJsonUpdate();
+
+async function checkPackageJsonUpdate() {
+  const packageDiff = await exec(`git diff ORIG_HEAD HEAD --exit-code -- ${packageJsonPath}`);
+
+  if (packageDiff.stderr) {
+    throw new Error(packageDiff.stderr);
+  }
+  const updates = packageDiff.stdout;
+
+  console.log('updates: ', updates);
+}
